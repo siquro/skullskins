da5eb0c572409ef9fe04464a95df7af6743cf82c
Rol <rolikpolo34@gmail.com>
2023-05-28 16:42:06 +0300

    cors

--- back/app/main.ts
+++ back/app/main.ts
@@ -3,14 +3,11 @@ import { AppModule } from './models/app/app.module';
 import { APP_PORT } from '@st/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-const GATEWAY_CORE_WHITELIST = [
-  'http://localhost:3000'
-]
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: GATEWAY_CORE_WHITELIST,
+    origin: process.env.BASE_URL,
     credentials: true,
   });
 
